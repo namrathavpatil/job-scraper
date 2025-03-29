4e8286310be0ab92ec142e7c31caa5c9a2dae922
Namratha V Patil <46816441+namrathavpatil@users.noreply.github.com>
2025-03-28 17:17:33 -0700

    Update import_requests.py

--- import_requests.py
+++ import_requests.py
@@ -82,7 +82,7 @@ TARGET_COMPANIES = [
     "Sony", "Hitachi", "Tencent", "Huawei", "Deutsche Telekom", "Meta", "AT&T", "Alibaba", "Jingdong", "Foxconn",
     "Samsung Electronics", "Alphabet", "Apple", "Amazon", "Walmart", "UnitedHealth Group", "Berkshire Hathaway",
     "CVS Health", "ExxonMobil", "McKesson Corporation", "Cencora", "Costco", "JPMorgan Chase", "Cardinal Health",
-    "Chevron Corporation", "Cigna", "Ford Motor Company", "Bank of America", "General Motors", "Elevance Health"
+    "Chevron Corporation", "Cigna", "Ford Motor Company", "Bank of America", "General Motors", "Elevance Health","SoundCloud", "SharkNinja", "Juniper Networks", "Cisco ThousandEyes"
 ]
 
 def load_job_history():
